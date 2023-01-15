d052708cb4e9071f8859cdadde942ffc66ecfb91
Aditya Kane <adityakane1@gmail.com>
2023-01-15 14:53:51 +0530

    sync

--- Group D/PageReplacement/src/PageReplacement.java
+++ Group D/PageReplacement/src/PageReplacement.java
@@ -1,14 +1,19 @@
+import java.io.*;
+
 public class PageReplacement {
 
 	public static void main(String[] args) {
 		
 		FCFS fcfs=new FCFS();
+		System.out.println("FCFS");
 		//fcfs.execute();
 		
 		LRU lru=new  LRU();
+		System.out.println("LRU");
 		//lru.execute();
 		
 		Optimal optimal=new Optimal();
+		System.out.println("Optimal");
 		optimal.execute();
 	}
 
